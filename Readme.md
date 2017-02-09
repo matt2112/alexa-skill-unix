f727e56510feabf850fcf28f1a136add3e72425a
matt2112 <matt_lewis@live.co.uk>
2017-02-09 10:31:41 +0000

    set up Unix intent

--- Readme.md
+++ Readme.md
@@ -1,3 +1,11 @@
 # Unix Alexa Skill
 
-Custom Alexa skill for converting between natural dates and unix time - Say 'Alexa, tell Unix to get unix time'.
+Custom Alexa skill for converting between natural dates and unix time.
+
+### Examples
+
+'Alexa, tell Unix to get the unix time for today'.
+'Alexa, tell Unix to get the unix time for 9th February 2017'.
+'Alexa, tell Unix to convert 9th February 2017 to unix time'.
+
+'Alexa, tell Unix to get the natural date for today'.
--- index.js
+++ index.js
@@ -3,25 +3,44 @@
 const Alexa = require('alexa-sdk');
 const axios = require('axios');
 
+const APP_ID = 43;
+
 const handlers = {
     LaunchRequest: function () {
-        console.log('Launched');
+        // If no intent given, will call Unix function by default.
         this.emit('Unix');
     },
     Unix: function () {
-        axios.get('https://mdl-timestamp.herokuapp.com/December%2010,%202016')
+        const dateSlot = this.event.request.intent.slots.Date.value;
+        let date = new Date(dateSlot);
+        // If no date given or not able to parse, then use today's date as default.
+        if (date.toString() === 'Invalid Date') {
+            date = new Date();
+        }
+        console.log(`The date you requested is ${date}`);
+
+        const months = ['January', 'February', 'March', 'April', 'May', 'June',
+                        'July', 'August', 'September', 'October', 'November', 'December'];
+        // getMonth returns a number. Use as index for array above to get word required for API.
+        const month = months[date.getMonth()];
+        const day = date.getDate();
+        const year = date.getFullYear();
+        const dateString = `${month} ${day}, ${year}`;
+        console.log(`The datestring has been parsed to ${dateString}`);
+
+        axios.get(`https://mdl-timestamp.herokuapp.com/${dateString}`)
             .then((response) => {
                 const time = response.data.unix;
-                const speechOutput = `This is the unix time: ${time}.`;
+                const speechOutput = `The unix time for ${dateString} is ${time}.`;
                 this.emit(':tellWithCard', speechOutput, 'Unix time', time);
             })
             .catch((error) => {
                 console.log(error);
-                this.emit(':tell', 'There was an error from axios.');
+                this.emit(':tell', 'There was an error when attempting to access the timestamp API.');
             });
     },
     Natural: function () {
-        const date = 'a b c';
+        const date = '10th December, 2016';
         const speechOutput = `This is the natural date: ${date}.`;
         this.emit(':tellWithCard', speechOutput, 'Natural date', date);    
     }
@@ -29,6 +48,7 @@ const handlers = {
 
 exports.handler = (event, context) => {
     const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
