5865383aeb7fb05ab743369304271a2f8dd88890
matt2112 <matt_lewis@live.co.uk>
2017-03-02 13:45:30 +0000

    use built-in intents

--- lambda/index.js
+++ lambda/index.js
@@ -3,16 +3,19 @@
 const Alexa = require('alexa-sdk');
 const axios = require('axios');
 
+const REPROMPT = 'What would you like to do?';
+
 const handlers = {
+
   'LaunchRequest': function () {
-    console.log('TEST');
-        // If no intent given, will call Unix function by default.
-    this.emit(':tell', 'Unix');
+    // If no intent given, will call Unix function by default.
+    this.emit(':ask', 'Welcome to Unix Time.', REPROMPT);
   },
+
   'Unix': function () {
     const dateSlot = this.event.request.intent.slots.Date.value;
     let date = new Date(dateSlot);
-        // If no date given or not able to parse, then use today's date as default.
+    // If no date given or not able to parse, then use today's date as default.
     if (date.toString() === 'Invalid Date') {
       date = new Date();
     }
@@ -20,7 +23,7 @@ const handlers = {
 
     const months = ['January', 'February', 'March', 'April', 'May', 'June',
       'July', 'August', 'September', 'October', 'November', 'December'];
-        // getMonth returns a number. Use as index for array above to get word required for API.
+    // getMonth returns a number. Use as index for array above to get word required for API.
     const month = months[date.getMonth()];
     const day = date.getDate();
     const year = date.getFullYear();
@@ -38,25 +41,42 @@ const handlers = {
               this.emit(':tell', 'There was an error when attempting to access the timestamp API.');
             });
   },
-  // 'Natural': function () {
-  //   let time = this.event.request.intent.slots.Time.value;
-  //   console.log(`The time you requested is ${time}.`);
-  //   // If undefined, set time to 0 seconds i.e. January 1st, 1970.
-  //   if (!time) {
-  //     time = 0;
-  //   }
-  //
-  //   axios.get(`http://timestamp-microservice-dev.eu-west-2.elasticbeanstalk.com/${time}`)
-  //           .then((response) => {
-  //             const date = response.data.natural;
-  //             const speechOutput = `The natural date for ${time} seconds is ${date}.`;
-  //             this.emit(':tellWithCard', speechOutput, 'Natural date', date);
-  //           })
-  //           .catch((error) => {
-  //             console.log(error);
-  //             this.emit(':tell', 'There was an error when attempting to access the timestamp API.');
-  //           });
-  // },
+
+  'Natural': function () {
+    let time = this.event.request.intent.slots.Time.value;
+    console.log(`The time you requested is ${time}.`);
+    // If undefined, set time to 0 seconds i.e. January 1st, 1970.
+    if (!time) {
+      time = 0;
+    }
+
+    axios.get(`http://timestamp-microservice-dev.eu-west-2.elasticbeanstalk.com/${time}`)
+            .then((response) => {
+              const date = response.data.natural;
+              const speechOutput = `The natural date for ${time} seconds is ${date}.`;
+              this.emit(':tellWithCard', speechOutput, 'Natural date', date);
+            })
+            .catch((error) => {
+              console.log(error);
+              this.emit(':tell', 'There was an error when attempting to access the timestamp API.');
+            });
+  },
+
+  'AMAZON.CancelIntent': function () {
+    this.emit(':tell', 'Goodbye!');
+  },
+
+  'AMAZON.HelpIntent': function () {
+    this.emit(':ask', 'Ask me to convert between unix time and the natural date according to the Gregorian calendar.', REPROMPT);
+  },
+
+  'AMAZON.StopIntent': function () {
+    this.emit(':tell', 'Goodbye!');
+  },
+
+  'Unhandled': function () {
+    this.emit(':ask', 'Sorry, I didn\'t understand that request!', REPROMPT);
+  },
 };
 
 exports.handler = (event, context) => {
